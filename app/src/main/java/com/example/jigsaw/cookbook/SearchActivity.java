2348928ec48356f5bffbedcf47b6e6883e023b03
Shrung Bhatt <shrungbhatt@gmail.com>
2017-12-01 00:10:56 +0530

    Updated

--- app/src/main/java/com/example/jigsaw/cookbook/SearchActivity.java
+++ app/src/main/java/com/example/jigsaw/cookbook/SearchActivity.java
@@ -2,12 +2,44 @@ package com.example.jigsaw.cookbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchActivity";
+    private SearchView mIngredientSearchView;
+    private List<String> mIngredientsList;
+    private Button mParseStringButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mIngredientSearchView = findViewById(R.id.enter_ingredient_search_view);
+
+        mParseStringButton = findViewById(R.id.parse_search_string_button);
+        mParseStringButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mIngredientsList = parseString();
+                for(String ingredient : mIngredientsList){
+                    Log.i(TAG,ingredient);
+                }
+            }
+        });
+
+    }
+
+    private List<String> parseString(){
+        String ingredientsString = mIngredientSearchView.getQuery().toString().trim();
+        return Arrays.asList(ingredientsString.split(","));
     }
 }
