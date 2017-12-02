9e51d8e5bd70bcc70b0a44804cbe5c22fe151674
Shrung Bhatt <shrungbhatt@gmail.com>
2017-12-02 11:02:10 +0530

    Updated

--- app/src/main/java/com/example/jigsaw/cookbook/SearchActivity.java
+++ app/src/main/java/com/example/jigsaw/cookbook/SearchActivity.java
@@ -2,12 +2,12 @@ package com.example.jigsaw.cookbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.SearchView;
-
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,7 +16,7 @@ public class SearchActivity extends AppCompatActivity {
     private static final String TAG = "SearchActivity";
     private SearchView mIngredientSearchView;
     private List<String> mIngredientsList;
-    private Button mParseStringButton;
+    private RecyclerView mRecipeRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +25,35 @@ public class SearchActivity extends AppCompatActivity {
 
         mIngredientSearchView = findViewById(R.id.enter_ingredient_search_view);
 
-        mParseStringButton = findViewById(R.id.parse_search_string_button);
-        mParseStringButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mIngredientsList = parseString();
-                for(String ingredient : mIngredientsList){
-                    Log.i(TAG,ingredient);
-                }
-            }
-        });
+        mRecipeRecyclerView = findViewById(R.id.recipes_recyclerView);
+
 
     }
 
-    private List<String> parseString(){
-        String ingredientsString = mIngredientSearchView.getQuery().toString().trim();
-        return Arrays.asList(ingredientsString.split(","));
+
+    private class RecipeHolder extends RecyclerView.ViewHolder{
+
+        public RecipeHolder(View itemView) {
+            super(itemView);
+        }
     }
+
+    private class RecipeAdapter extends RecyclerView.Adapter<RecipeHolder>{
+
+        @Override
+        public RecipeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecipeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
 }
