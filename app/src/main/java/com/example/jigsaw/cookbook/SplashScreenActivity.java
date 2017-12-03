e2031e75dafa4e6a6573d958ef93cd801a9b3f97
Shrung Bhatt <shrungbhatt@gmail.com>
2017-12-03 10:15:13 +0530

    Added Splash Screen Activity

---
+++ app/src/main/java/com/example/jigsaw/cookbook/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.jigsaw.cookbook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activtiy_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent =
+                        new Intent(SplashScreenActivity.this,SearchActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, 1000);
+    }
+
+}
