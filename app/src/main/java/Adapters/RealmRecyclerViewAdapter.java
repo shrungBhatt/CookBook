939918fe7b32861daa541e0bbd237ec10e55247a
Shrung Bhatt <shrungbhatt@gmail.com>
2018-02-11 11:54:43 +0530

    Final Commit

--- app/src/main/java/Adapters/RealmRecyclerViewAdapter.java
+++ app/src/main/java/Adapters/RealmRecyclerViewAdapter.java
@@ -9,12 +9,12 @@ public abstract class RealmRecyclerViewAdapter<T extends RealmObject> extends Re
 
     private RealmBaseAdapter<T> realmBaseAdapter;
 
-    public T getItem(int position) {
+    T getItem(int position) {
 
         return realmBaseAdapter.getItem(position);
     }
 
-    public RealmBaseAdapter<T> getRealmAdapter() {
+    RealmBaseAdapter<T> getRealmAdapter() {
 
         return realmBaseAdapter;
     }
